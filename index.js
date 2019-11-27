77d22c49cf2b92321f3057e0b17d46972cbbe111
confident-history-3187 <boris.matovic@gmail.com>
2019-11-27 19:29:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function destructivelyRemoveLastKitten(){
   return kittens.pop()
 }
 
-function destructivelyremoveFirstKitten(){
+function destructivelyRemoveFirstKitten(){
   return kittens.shift()
 }
